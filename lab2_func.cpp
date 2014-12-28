3d3b52d1259ebe72ccf9748d62d3e30ca52d6d03
Abdul Wahab <arwahab@users.noreply.github.com>
2014-12-27 19:30:14 -0500

    Create lab2_func.cpp

---
+++ lab2_func.cpp
@@ -0,0 +1,29 @@
+#include "lab2_func.h"
+
+// Abdul Wahab
+// Data Structures 
+
+using namespace std;
+
+int add (int i1, int i2) {
+	return (i1 + i2);
+}
+
+int subtract (int i1, int i2){
+	return (i1 - i2);
+
+}
+
+int multiply (int i1, int i2){
+	return (i1 * i2);
+
+}
+
+int divide (int i1, int i2){
+	return (i1 / i2);
+
+}
+
+
+// TO-DO: implement the code for subtract, multiply, and divide functions.
+//		  make sure headers match how you declare them in header file
