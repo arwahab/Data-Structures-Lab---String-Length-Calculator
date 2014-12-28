9ba4fedd28103f771f04852af28285ba2253fbeb
Abdul Wahab <arwahab@users.noreply.github.com>
2014-12-27 19:29:09 -0500

    Create lab2_func.h

---
+++ lab2_func.h
@@ -0,0 +1,15 @@
+#ifndef _LAB2_FUNC_H_
+#define _LAB2_FUNC_H_
+
+// Abdul Wahab
+// Data Structures
+
+// add the 2 integer arguments and return the result
+int add (int i1, int i2);
+
+// TO-DO: create headers for subtract, multiply, and divide functions
+int subtract (int i1, int i2);
+int multiply (int i1, int i2);
+int divide (int i1, int i2);
+
+#endif
